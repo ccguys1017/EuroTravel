04e654faa79f1c10c117a29c4b1d90bdd76b76cb
toneloke <david.guerrero87@live.com>
2017-07-23 13:15:19 -0400

    added devtools

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import reduxThunk from 'redux-thunk'
 import App from './components/app'
 import reducers from './reducers'
 import { AUTH_USER} from './actions/types'
+import DevTools from './devTools'
 
 // Add middleware to your store
-const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
+const createStoreWithMiddleware = applyMiddleware(reduxThunk, DevTools.instrument())(createStore)
 // create your store with the middleware added
 const store = createStoreWithMiddleware(reducers)
 
