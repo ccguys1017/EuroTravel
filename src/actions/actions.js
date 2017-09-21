9ee9d2313f7da4b11fce3a712a986345a4b383fe
ccguys1017 <codercampsguys1017>
2017-09-21 08:05:39 -0700

    removed unnecessary src/actions/actions.js file

--- src/actions/actions.js
+++
@@ -1,4 +0,0 @@
-import * as types from './types';
-
-export const insertMap = (lat, lng) => ({type: types.INSERT_MAP,lat, lng});
-export const locationClicked = () => ({type: types.LOCATION_CLICKED});
