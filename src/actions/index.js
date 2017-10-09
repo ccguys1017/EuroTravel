cffa49f8ee025e5cfb5164d46c461dc286866ac4
ccguys1017 <31857783+ccguys1017@users.noreply.github.com>
2017-10-09 16:37:06 -0700

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,7 +10,7 @@ import {
   WIPE_PLACES
 } from './types';
 import * as types from './types';
-const ROOT_URL = 'http://localhost:8080/api/v1';
+const ROOT_URL = 'https://eurotravel-sever.herokuapp.com/api/v1';
 
 export function signinUser ({ email, password }) {
   return function (dispatch) {
