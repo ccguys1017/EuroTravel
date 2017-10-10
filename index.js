4ee140337dc84ee1ee611eea2a529a6750370f81
ccguys1017 <31857783+ccguys1017@users.noreply.github.com>
2017-10-10 07:14:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -32,5 +32,6 @@ app.get('*', function(req, res) {
 // Server Setup
 const port = process.env.PORT || 8080;
 const server = http.createServer(app);
+server.listen(port);
 console.log('Server listening on:', port);
 
