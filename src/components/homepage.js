4317190f6ce9da3c2df7d4dfd8566070cac89b17
ccguys1017 <31857783+ccguys1017@users.noreply.github.com>
2017-10-16 19:40:20 -0700

    footer updates

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -44,7 +44,7 @@ class Homepage extends Component {
   render () {
     return (
       <div className='tg-header'>
-        <nav className="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar">
+        <nav className="navbar navbar-fixed-top">
           <div className="container-fluid">
             <div className="navbar-header">
               <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -68,63 +68,71 @@ class Homepage extends Component {
           </div>
         </nav>
 
-        <div id='video-carousel-example2' className='carousel slide carousel-fade' data-ride='carousel'>
-          <ol className='carousel-indicators'>
-            <li data-target='#video-carousel-example2' data-slide-to='0' className='active'></li>
-            
-          </ol>
+        <div
+        id="video-carousel-example2"
+        className="carousel slide"
+        data-ride="carousel"
+      >
+        <div className="carousel-inner" role="listbox">
+          <div className="carousel-item active">
+            <div className="view hm-purple-slight">
+              <Video className="video" controls autoPlay loop>
+                <source
+                  src="https://mdbootstrap.com/img/video/Tropical.mp4"
+                  type="video/mp4"
+                />
+              </Video>
+              <div className="full-bg-img" />
+            </div>
+            <div className="carousel-caption">
+              <div className="flex-center animated fadeIn">
+                <ul id="home" style={{ "list-style-type": "none" }}>
+                  <li>
+                    <h1 className="h1-responsive">Guide Trip</h1>
+                  </li>
+                  <li>
+                    <p>your final destination for all your travel needs</p>
+                  </li>
 
-          <div className='carousel-inner' role='listbox'>
-            <div className='carousel-item active'>
-              <div className='view hm-purple-slight'>
-                <Video className='video-full' controls autoPlay loop>
-                    <source src='https://mdbootstrap.com/img/video/Tropical.mp4' type='video/mp4' />
-                </Video>
-                <div className='full-bg-img'></div>
-              </div>
-              <div className='carousel-caption'>
-                <div className='flex-center animated fadeIn'>
-                  <ul id="home" style={{"list-style-type": "none"}}>
-                    <li><h1 className='h1-responsive'>Guide Trip</h1></li>
-                    <li><p>Guide Trip is your final destination for all your travel needs</p></li>
-                    
-                    <div id="homeBTN">
+                  <div id="homeBTN">
                     <Col xs={4} md={6}>
-                    <li ><a href='/signup' className='btn btn-primary rounded waves-effect' rel='nofollow'>Sign up!</a></li>
+                      <li>
+                        <a
+                          href="/signup"
+                          className="btn btn-primary rounded waves-effect"
+                          rel="nofollow"
+                        >
+                          Sign up!
+                        </a>
+                      </li>
                     </Col>
-                    <li ><a  href='/signin' className='btn btn-primary rounded waves-effect' rel='nofollow'>Log In!</a></li>
-                    
-                    </div>
-                  </ul>
-                </div>
+                    <li>
+                      <a
+                        href="/signin"
+                        className="btn btn-primary rounded waves-effect"
+                        rel="nofollow"
+                      >
+                        Log In!
+                      </a>
+                    </li>
+                  </div>
+                </ul>
               </div>
             </div>
+
+            <footer className="absolute">
+              <div class="footer-copyright">
+                &copy; {new Date().getFullYear()} Copyright:{" "}
+                <a href="https://www.guidetrip.me"> www.guidetrip.me </a>
+              </div>
+            </footer>
           </div>
-          <a className='carousel-control-prev' href='#video-carousel-example2' role='button' data-slide='prev'>
-          <span className='carousel-control-prev-icon' aria-hidden='true'></span>
-          <span className='sr-only'>Previous</span></a>
-          <a className='carousel-control-next' href='#video-carousel-example2' role='button' data-slide='next'>
-          <span className='carousel-control-next-icon' aria-hidden='true'></span>
-          <span className='sr-only'>Next</span></a>
         </div>
-
-        <footer style={{position:"relative"}}id="sticky">
-        
-          <a href="/"> Home</a>
-          <a href="/dashboard"> Dashboard</a>
-          <a href="/hotelBuild"> Find Hotels</a>
-        
-        <div class="footer-copyright">
-    <div class="container-fluid">
-        © 2017 Copyright: <a href="/"> GuideTrip </a>
-
+      </div>
     </div>
-</div>
-  </footer>
-    </div>
-    );
-  }
-};
+  );
+}
+}
 
 function mapStateToProps (state) {
   return {
@@ -134,3 +142,4 @@ function mapStateToProps (state) {
 };
 
 export default connect(mapStateToProps, actions)(Homepage);
+
