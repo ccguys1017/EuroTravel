ff6f5ee0b85cbc774451a956c56399789160850c
ccguys1017 <codercampsguys@gmail.com>
2017-10-17 09:45:55 -0700

    removed unnecessary temp.js file

--- client/src/components/temp.js
+++
@@ -1,7 +0,0 @@
-/*
-<div className='col-md-1 columns'>
-                <label className='checkbox-inline'>
-                    <input type='checkbox' onClick={this.selectAll(this)}/><strong>Check All
-                </strong></label>
-            </div>
-*/
