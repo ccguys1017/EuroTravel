9b7b46a99d4931b0c67cd05423e676854c6d463f
airjulius <airjulius@gmail.com>
2017-10-18 17:54:34 -0700

    stylinfordemo

--- client/src/components/homepage.js
+++ client/src/components/homepage.js
@@ -1,65 +1,93 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { DefaultPlayer as Video } from 'react-html5video';
-import * as actions from '../actions';
-import PropTypes from 'prop-types';
-import {Col} from 'react-bootstrap';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { DefaultPlayer as Video } from "react-html5video";
+import * as actions from "../actions";
+import PropTypes from "prop-types";
+import { Col } from "react-bootstrap";
 
 class Homepage extends Component {
   static contextTypes = {
     router: PropTypes.object
   };
 
-  componentDidMount () {
-    this.props.fetchMessage()
-  };
+  componentDidMount() {
+    this.props.fetchMessage();
+  }
 
-  renderLinks () {
+  renderLinks() {
     if (this.props.authenticated) {
       // show a link for user to go to Dashboard or Sign Out
       return [
-        <li className='nav-item' key={1}>
-          <Link className='btn btn-default tg-login__btn' to='/dashboard'><strong>Dashboard</strong></Link>
-        </li>
-        ,
-        <li className='nav-item' key={2}>
-          <Link className='nav-link' to='/signout'><strong>Sign Out</strong></Link>
+        <li className="nav-item" key={1}>
+          <Link className="btn btn-default tg-login__btn" to="/dashboard">
+            <strong>Dashboard</strong>
+          </Link>
+        </li>,
+        <li className="nav-item" key={2}>
+          <Link className="nav-link" to="/signout">
+            <strong>Sign Out</strong>
+          </Link>
         </li>
       ];
     } else {
       // show a link for user to Sign In or Sign Up
       return [
-        <li className='nav-item'>
-          <Link className='btn btn-default tg-login__btn' to='/signin' key={1}>Sign In</Link>
-        </li>
-        ,
-        <li className='nav-item' key={2}>
-          <Link className='btn btn-default tg-login__btn' to='/signup'>Sign Up</Link>
+        <li className="nav-item">
+          <Link className="btn btn-default tg-login__btn" to="/signin" key={1}>
+            Sign In
+          </Link>
+        </li>,
+        <li className="nav-item" key={2}>
+          <Link className="btn btn-default tg-login__btn" to="/signup">
+            Sign Up
+          </Link>
         </li>
       ];
     }
-  };
+  }
 
-  render () {
+  render() {
     return (
-      <div className='tg-header'>
+      <div className="tg-header">
         <nav className="navbar navbar-fixed-top">
           <div className="container-fluid">
             <div className="navbar-header">
-              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <button
+                type="button"
+                className="navbar-toggle collapsed"
+                data-toggle="collapse"
+                data-target="#bs-example-navbar-collapse-1"
+                aria-expanded="false"
+              >
                 <span className="sr-only">Toggle navigation</span>
-                <span className="icon-bar"></span>
+                <span className="icon-bar" />
               </button>
               <a className="navbar-brand" href="#">
-              <strong>Guide Trip</strong></a>
+                <strong>Guide Trip</strong>
+              </a>
             </div>
-            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <div
+              className="collapse navbar-collapse"
+              id="bs-example-navbar-collapse-1"
+            >
               <ul className="nav navbar-nav">
-                <li className="active"><a href="/"><strong>Home</strong><span className="sr-only">(current)</span></a></li>
-                <li><a href="#"><strong>Trip Ideas</strong></a></li>
-                <li><a href="#"><strong>Destinations</strong></a></li>
-
+                <li className="active">
+                  <a href="/">
+                    <strong>Home</strong>
+                    <span className="sr-only">(current)</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../views/index.html">
+                    <strong>AboutUs</strong>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <strong>Destinations</strong>
+                  </a>
+                </li>
               </ul>
               <ul className="nav navbar-nav navbar-right">
                 {this.renderLinks()}
@@ -69,76 +97,76 @@ class Homepage extends Component {
         </nav>
 
         <div
-        id="video-carousel-example2"
-        className="carousel slide"
-        data-ride="carousel"
-      >
-        <div className="carousel-inner" role="listbox">
-          <div className="carousel-item active">
-            <div className="view hm-purple-slight">
-              <Video className="video" controls autoPlay loop>
-                <source
-                  src="https://mdbootstrap.com/img/video/Tropical.mp4"
-                  type="video/mp4"
-                />
-              </Video>
-              <div className="full-bg-img" />
-            </div>
-            <div className="carousel-caption">
-              <div className="flex-center animated fadeIn">
-                <ul id="home" style={{ "list-style-type": "none" }}>
-                  <li>
-                    <h1 className="h1-responsive">Guide Trip</h1>
-                  </li>
-                  <li>
-                    <p>your final destination for all your travel needs</p>
-                  </li>
+          id="video-carousel-example2"
+          className="carousel slide"
+          data-ride="carousel"
+        >
+          <div className="carousel-inner" role="listbox">
+            <div className="carousel-item active">
+              <div className="view hm-purple-slight">
+                <Video className="video" controls autoPlay loop>
+                  <source
+                    src="https://mdbootstrap.com/img/video/Tropical.mp4"
+                    type="video/mp4"
+                  />
+                </Video>
+                <div className="full-bg-img" />
+              </div>
+              <div className="carousel-caption">
+                <div className="flex-center animated fadeIn">
+                  <ul id="home" style={{ "list-style-type": "none" }}>
+                    <li>
+                      <h1 className="h1-responsive">Guide Trip</h1>
+                    </li>
+                    <li>
+                      <p>your final destination for all your travel needs</p>
+                    </li>
 
-                  <div id="homeBTN">
-                    <Col xs={4} md={6}>
+                    <div id="homeBTN">
+                      <Col xs={4} md={6}>
+                        <li>
+                          <a
+                            href="/signup"
+                            className="btn btn-primary rounded waves-effect"
+                            rel="nofollow"
+                          >
+                            Sign up!
+                          </a>
+                        </li>
+                      </Col>
                       <li>
                         <a
-                          href="/signup"
+                          href="/signin"
                           className="btn btn-primary rounded waves-effect"
                           rel="nofollow"
                         >
-                          Sign up!
+                          Log In!
                         </a>
                       </li>
-                    </Col>
-                    <li>
-                      <a
-                        href="/signin"
-                        className="btn btn-primary rounded waves-effect"
-                        rel="nofollow"
-                      >
-                        Log In!
-                      </a>
-                    </li>
-                  </div>
-                </ul>
+                    </div>
+                  </ul>
+                </div>
               </div>
-            </div>
 
-            <footer className="absolute">
-              <div class="footer-copyright">
-                &copy; {new Date().getFullYear()} Copyright:{" "}
-                <a href="https://www.guidetrip.me"> www.guidetrip.me </a>
-              </div>
-            </footer>
+              <footer className="absolute">
+                <div class="footer-copyright">
+                  &copy; {new Date().getFullYear()} Copyright:{" "}
+                  <a href="https://www.guidetrip.me"> www.guidetrip.me </a>
+                </div>
+              </footer>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 }
 
-function mapStateToProps (state) {
+function mapStateToProps(state) {
   return {
     authenticated: state.auth.authenticated,
     message: state.auth.message
   };
-};
+}
 
 export default connect(mapStateToProps, actions)(Homepage);
