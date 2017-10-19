6a2bd1c7f359cec515a78baf3a7588e35d4ac88b
ccguys1017 <31857783+ccguys1017@users.noreply.github.com>
2017-10-19 10:07:20 -0700

    Update manualSearch.js

--- src/components/manualSearch.js
+++ src/components/manualSearch.js
@@ -53,6 +53,7 @@ class manualSearch extends React.Component{
   )
 
   onButtonClick () {
+    localStorage.setItem('hotel_flag', true);
     this.context.router.history.push('/hotelSearch');
   };
 
@@ -357,55 +358,61 @@ class manualSearch extends React.Component{
     <h3 style={{textAlign: "center"}}><strong>Your Custom Itinerary Results for: </strong><span>{ cb_city}, {cb_country}</span></h3>
         <h4 style={{textAlign: "center"}}><strong>Marker Legend</strong></h4>
         <div className='row1'>
-          <div className='col-md-12 columns'>
+        <div className='col-md-12 columns'>
+        <div className='col-md-2 columns'>
+          </div>
+        <div className='col-md-1 columns'>
+          </div>
           <div className='col-md-1 columns'>
-            </div>
-            <div className='col-md-1 columns'>
-              <span className="badge" id='testA'>A - Store</span>
-              <span className="badge" id='testB'>B - Lodging</span>
-              <span className="badge" id='testC'>C - Restaurant/Cafe</span>
-              <span className="badge" id='testD'>D - Museum/Art Gallery</span>
-              <span className="badge" id='testE'>E - Pharmacy</span>
-              <span className="badge" id='testF'>F - Subway</span>
-            </div>
-            <div className='col-md-1 columns'>
-            </div>
-            <div className='col-md-1 columns'>
-              <span className="badge" id='testG'>G - Airport</span>
-              <span className="badge" id='testH'>H - Hospital</span>
-              <span className="badge" id='testI'>I - Bus</span>
-              <span className="badge" id='testJ'>J - Park</span>
-              <span className="badge" id='testK'>K - ATM</span>
-              <span className="badge" id='testL'>L - Bank</span>
-            </div>
-            <div className='col-md-1 columns'>
-            </div>
-            <div className='col-md-1 columns'>
-              <span className="badge" id='testM'>M - Doctor/Dentist</span>
-              <span className="badge" id='testN'>N - Zoo</span>
-              <span className="badge" id='testO'>O - Police</span>
-              <span className="badge" id='testP'>P - Train</span>
-              <span className="badge" id='testQ'>Q - School</span>
-              <span className="badge" id='testR'>R - Bar</span>
-            </div>
-            <div className='col-md-1 columns'>
-            </div>
-            <div className='col-md-1 columns'>
-              <span className="badge" id='testS'>S - Church</span>
-              <span className="badge" id='testT'>T - Synagogue</span>
-              <span className="badge" id='testU'>U - Mosque</span>
-              <span className="badge" id='testV'>V - University</span>
-              <span className="badge" id='testW'>W - Embassy</span>
-              <span className="badge" id='testX'>X - Library</span>
-            </div>
-            <div className='col-md-1 columns'>
-            </div>
-            <div className='col-md-1 columns'>
-              <span className="badge" id='testY'>Y - Spa</span>
-              <span className="badge" id='testZ'>Z - Other</span>
-            </div>
+            <span className="badge" id='testA'>A - Store</span>
+            <span className="badge" id='testB'>B - Lodging</span>
+            <span className="badge" id='testC'>C - Restaurant/Cafe</span>
+            <span className="badge" id='testD'>D - Museum/Art Gallery</span>
+            <span className="badge" id='testE'>E - Pharmacy</span>
+            <span className="badge" id='testF'>F - Subway</span>
+          </div>
+          <div className='col-md-1 columns'>
+          </div>
+          <div className='col-md-1 columns'>
+            <span className="badge" id='testG'>G - Airport</span>
+            <span className="badge" id='testH'>H - Hospital</span>
+            <span className="badge" id='testI'>I - Bus</span>
+            <span className="badge" id='testJ'>J - Park</span>
+            <span className="badge" id='testK'>K - ATM</span>
+            <span className="badge" id='testL'>L - Bank</span>
+          </div>
+          <div className='col-md-1 columns'>
+          </div>
+          <div className='col-md-1 columns'>
+            <span className="badge" id='testM'>M - Doctor/Dentist</span>
+            <span className="badge" id='testN'>N - Zoo</span>
+            <span className="badge" id='testO'>O - Police</span>
+            <span className="badge" id='testP'>P - Train</span>
+            <span className="badge" id='testQ'>Q - School</span>
+            <span className="badge" id='testR'>R - Bar</span>
+          </div>
+          <div className='col-md-1 columns'>
+          </div>
+          <div className='col-md-1 columns'>
+            <span className="badge" id='testS'>S - Church</span>
+            <span className="badge" id='testT'>T - Synagogue</span>
+            <span className="badge" id='testU'>U - Mosque</span>
+            <span className="badge" id='testV'>V - University</span>
+            <span className="badge" id='testW'>W - Embassy</span>
+            <span className="badge" id='testX'>X - Library</span>
+          </div>
+          <div className='col-md-1 columns'>
+          </div>
+          </div>
+          <div className='col-md-12 columns'>
+        <div className='col-md-5 columns'>
+          </div>
+          <div className='col-md-1 columns'>
+            <span className="badge" id='testY'>Y - Spa</span>
+            <span className="badge" id='testZ'>Z - Other</span>
           </div>
         </div>
+      </div>
         <div id="map">
           </div>
         <div className="row">
