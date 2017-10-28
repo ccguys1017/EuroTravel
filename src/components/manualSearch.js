896eaad8ee5a7125dc378bfdf850c48fd63c30aa
ccguys1017 <31857783+ccguys1017@users.noreply.github.com>
2017-10-28 12:34:53 -0700

    added current weather functionality

--- src/components/manualSearch.js
+++ src/components/manualSearch.js
@@ -10,32 +10,133 @@ import axios from 'axios';
 import {Table, Nav, Navbar, NavItem} from 'react-bootstrap';
 
 const ROOT_URL = 'https://eurotravel-sever.herokuapp.com/api/v1';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather';
+const OPEN_WEATHER_MAP_ICON = 'http://api.openweathermap.org/img/w/';
 
 class manualSearch extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-          itins_saved: false,
-        };
-    }
-    static contextTypes = {
-      router: PropTypes.object
+  constructor(props){
+    super(props)
+    this.state = {
+      itins_saved: false,
+      weather_descr: '',
+      weather_temp: 0,
+      weather_pressure: 0,
+      weather_humidity: 0,
+      weather_speed: 0,
+      weather_clouds: 0,
+      weather_deg: 0,
+      weather_direction_0: 'NNE',
+      weather_direction_1: 'NE',
+      weather_direction_2: 'ENE',
+      weather_direction_3: 'E',
+      weather_direction_4: 'ESE',
+      weather_direction_5: 'SE',
+      weather_direction_6: 'SSE',
+      weather_direction_7: 'S',
+      weather_direction_8: 'SSW',
+      weather_direction_9: 'SW',
+      weather_direction_A: 'WSW',
+      weather_direction_B: 'W',
+      weather_direction_C: 'WNW',
+      weather_direction_D: 'NW',
+      weather_direction_E: 'NNW',
+      weather_direction_F: 'N',
+      weather_sunrise: '',
+      weather_sunset: '',
+      sunrise: '',
+      sunset: '',
+      datetime: ''
     };
+  }
 
+  static contextTypes = {
+    router: PropTypes.object
+  };
 
-    createCheckbox = (name, place_id) => (
-      <Checkbox
-        label={name}
-        handleCheckboxChange={this.toggleCheckbox}
-        key={place_id}
-        handleSave={this.handleSave}
-      />)
+  createCheckbox = (name, place_id) => (
+    <Checkbox
+      label={name}
+      handleCheckboxChange={this.toggleCheckbox}
+      key={place_id}
+      handleSave={this.handleSave}
+    />)
 
       componentWillMount = () => {
         this.selectedCheckboxes = new Set();
         this.placesForAllTypes = new Set();
     
         console.log(this.props);
+        let weatherDataUrl = '';
+
+        if (localStorage.getItem('local_flag')) {
+          weatherDataUrl = `${OPEN_WEATHER_MAP_URL}?q=&lon=${Number(localStorage.getItem('longitude'))}&lat=${Number(localStorage.getItem('latitude'))}&APPID=144d3c403202aaa5edea7d230772a01d`;
+        } else {
+          weatherDataUrl = `${OPEN_WEATHER_MAP_URL}?q=&lon=${Number(this.props.state.maps.selectedLocation.lng)}&lat=${Number(this.props.state.maps.selectedLocation.lat)}&APPID=144d3c403202aaa5edea7d230772a01d`;
+        }
+        
+        axios.get(weatherDataUrl)
+          .then(response => {
+            localStorage.setItem('weather_descr', JSON.stringify(response.data.weather[0].description));
+            localStorage.setItem('weather_temp', JSON.stringify(response.data.main.temp));
+            localStorage.setItem('weather_pressure', JSON.stringify(response.data.main.pressure));
+            localStorage.setItem('weather_humidity', JSON.stringify(response.data.main.humidity));
+            localStorage.setItem('weather_clouds', JSON.stringify(response.data.clouds.all));
+            localStorage.setItem('weather_time', JSON.stringify(response.data.dt));
+            localStorage.setItem('weather_speed', JSON.stringify(response.data.wind.speed));
+            localStorage.setItem('weather_deg', JSON.stringify(response.data.wind.deg));
+            localStorage.setItem('weather_sunrise', JSON.stringify(response.data.sys.sunrise));
+            localStorage.setItem('weather_sunset', JSON.stringify(response.data.sys.sunset));
+          });
+
+
+            if (localStorage.getItem('weather_deg') >= 0 && localStorage.getItem('weather_deg') < 22.5) {
+              localStorage.setItem('weather_direction', this.state.weather_direction_0);
+            } else 
+            if (localStorage.getItem('weather_deg') >= 22.5 && localStorage.getItem('weather_deg') < 45.0) {
+              localStorage.setItem('weather_direction', this.state.weather_direction_1);
+            } else 
+            if (localStorage.getItem('weather_deg') >= 45.0 && localStorage.getItem('weather_deg') < 67.5) {
+              localStorage.setItem('weather_direction', this.state.weather_direction_2);
+            } else 
+            if (localStorage.getItem('weather_deg') >= 67.5 && localStorage.getItem('weather_deg') < 90.0) {
+              localStorage.setItem('weather_direction', this.state.weather_direction_3);
+            } else 
+            if (localStorage.getItem('weather_deg') >= 90.0 && localStorage.getItem('weather_deg') < 112.5) {
+              localStorage.setItem('weather_direction', this.state.weather_direction_4);
+            } else 
+            if (localStorage.getItem('weather_deg') >= 112.5 && localStorage.getItem('weather_deg') < 135.0) {
+              localStorage.setItem('weather_direction', this.state.weather_direction_5);
+            } else 
+            if (localStorage.getItem('weather_deg') >= 135.0 && localStorage.getItem('weather_deg') < 157.5) {
+              localStorage.setItem('weather_direction', this.state.weather_direction_6);
+            } else 
+            if (localStorage.getItem('weather_deg') >=157.5 && localStorage.getItem('weather_deg') < 180.0) {
+              localStorage.setItem('weather_direction', this.state.weather_direction_7);
+            } else 
+            if (localStorage.getItem('weather_deg') >= 180.0 && localStorage.getItem('weather_deg') < 202.5) {
+              localStorage.setItem('weather_direction', this.state.weather_direction_8);
+            } else 
+            if (localStorage.getItem('weather_deg') >= 202.5 && localStorage.getItem('weather_deg') < 225.0) {
+              localStorage.setItem('weather_direction', this.state.weather_direction_9);
+            } else 
+            if (localStorage.getItem('weather_deg') >= 225.0 && localStorage.getItem('weather_deg') < 247.5) {
+              localStorage.setItem('weather_direction', this.state.weather_direction_A);
+            } else 
+            if (localStorage.getItem('weather_deg') >= 247.5 && localStorage.getItem('weather_deg') < 270.0) {
+              localStorage.setItem('weather_direction', this.state.weather_direction_B);
+            } else 
+            if (localStorage.getItem('weather_deg') >= 270.0 && localStorage.getItem('weather_deg') < 292.5) {
+              localStorage.setItem('weather_direction', this.state.weather_direction_C);
+            } else 
+            if (localStorage.getItem('weather_deg') >= 292.5 && localStorage.getItem('weather_deg') < 315.0) {
+              localStorage.setItem('weather_direction', this.state.weather_direction_D);
+            } else 
+            if (localStorage.getItem('weather_deg') >= 315.0 && localStorage.getItem('weather_deg') < 337.5) {
+              localStorage.setItem('weather_direction', this.state.weather_direction_E);
+            } else 
+            if (localStorage.getItem('weather_deg') >= 337.5 && localStorage.getItem('weather_deg') < 360.0) {
+              localStorage.setItem('weather_direction', this.state.weather_direction_F);
+            }
       }
     
       toggleCheckbox = label => {
@@ -47,13 +148,10 @@ class manualSearch extends React.Component{
       }
 
   createCheckboxes = () => (
-    
     this.props.state.maps.places.map(this.createCheckbox)
-    
   )
 
   onButtonClick () {
-    localStorage.setItem('hotel_flag', true);
     this.context.router.history.push('/hotelSearch');
   };
 
@@ -95,8 +193,27 @@ class manualSearch extends React.Component{
         });        
       })
     }
-
     this.context.router.history.push('/dashboard');
+  }
+
+  timeConverter(UNIX_sunrise, UNIX_sunset, UNIX_datetime){
+    var a = new Date(UNIX_sunrise*1000);
+    var hour_a = a.getUTCHours()+2;
+    var min_a = a.getUTCMinutes();
+    var time_a = hour_a + ':' + min_a ;
+    localStorage.setItem('sunrise', time_a);
+
+    var b = new Date(UNIX_sunset*1000);
+    var hour_b = b.getUTCHours()+2;
+    var min_b = b.getUTCMinutes();
+    var time_b = hour_b + ':' + min_b ;
+    localStorage.setItem('sunset', time_b);
+
+    var c = new Date(UNIX_datetime*1000);
+    var hour_c = c.getUTCHours();
+    var min_c = c.getUTCMinutes();
+    var time_c = hour_c + ':' + min_c ;
+    localStorage.setItem('datetime', time_c);
   }
 
     componentDidMount() {
@@ -131,6 +248,7 @@ class manualSearch extends React.Component{
               
           });
         }        
+
         service.getDetails({
           placeId: this.props.state.maps.selectedLocation.place_id
         }, function(place, status) {
@@ -139,7 +257,6 @@ class manualSearch extends React.Component{
             // Intended behavior is to set this.setState({places.place.reviews})
           }
         });
-        
         
         var x = 0; //Counter for info marker open/close
         function createMarker(place, x) {
@@ -362,7 +479,22 @@ class manualSearch extends React.Component{
         <h4 style={{textAlign: "center"}}><strong>Marker Legend</strong></h4>
         <div className='row1'>
         <div className='col-md-12 columns'>
-        <div className='col-md-2 columns'>
+        <div className='col-md-2 columns'>              
+
+        {this.timeConverter(localStorage.getItem('weather_sunrise'), localStorage.getItem('weather_sunset'), localStorage.getItem('weather_time'))}
+        <span><strong>Weather Conditions at: {localStorage.getItem('datetime')}</strong>
+          <ul>
+            <li><strong><span>{localStorage.getItem('weather_descr')}</span></strong></li>
+            <li><strong><span>Temp: {(1.8*localStorage.getItem('weather_temp') - 459.67).toFixed(1)} F / {((localStorage.getItem('weather_temp') - 273).toFixed(1))} C</span></strong></li>
+            <li><strong><span>Cloudiness: {localStorage.getItem('weather_clouds')}%</span></strong></li>
+            <li><strong><span>Baro: {localStorage.getItem('weather_pressure')} mbars</span></strong></li>
+            <li><strong><span>Humidity: {localStorage.getItem('weather_humidity')}%</span></strong></li>
+            <li><strong><span>Wind Speed: {(2.23694 * localStorage.getItem('weather_speed')).toFixed(2)} mi/hr</span></strong></li>
+            <li><strong><span>Wind Direction: {localStorage.getItem('weather_direction')}</span></strong></li>
+            <li><strong><span>Sunrise: {localStorage.getItem('sunrise')} am</span></strong></li>
+            <li><strong><span>Sunset: {localStorage.getItem('sunset')} pm</span></strong></li>
+          </ul>
+        </span>
           </div>
         <div className='col-md-1 columns'>
           </div>
@@ -460,3 +592,4 @@ function mapDispatchToProps(dispatch){
 manualSearch = connect(mapStateToProps, mapDispatchToProps)(manualSearch);
       
 export default manualSearch;
+
