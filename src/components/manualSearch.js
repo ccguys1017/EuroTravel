626c142a2f16b9987953f617e87a321b3ee7a27b
ccguys1017 <31857783+ccguys1017@users.noreply.github.com>
2017-10-29 17:37:07 -0700

    Update manualSearch.js

--- src/components/manualSearch.js
+++ src/components/manualSearch.js
@@ -132,6 +132,7 @@ class manualSearch extends React.Component{
       if (localStorage.getItem('weather_deg') >= 337.5 && localStorage.getItem('weather_deg') < 360.0) {
         localStorage.setItem('weather_direction', this.state.weather_direction_F);
       }
+      this.setState({ weather_loaded: true });
   }
 
   componentWillMount = () => {
